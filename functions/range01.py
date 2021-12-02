e754b30dc7aee2108a71f558dfe996a8e6911d0b
alexjcwitsil <alexjcwitsil@gmail.com>
2021-12-02 12:54:40 -0500

    add range01 function

---
+++ functions/range01.py
@@ -0,0 +1,15 @@
+#############################
+### RANGE BETWEEN 0 and 1 ###
+#############################
+
+import numpy as np
+
+def range01(x):
+    x_out = (x-np.min(x)) / (np.max(x) - np.min(x))
+    return(x_out)
+
+
+
+
+
+
