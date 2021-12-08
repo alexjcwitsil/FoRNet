cfeca6466885e01ab78d22d89a9fa043529332b6
alexjcwitsil <alexjcwitsil@gmail.com>
2021-12-08 18:58:24 -0500

    add handle for small arrays where min(array)==max(array)

--- functions/range01.py
+++ functions/range01.py
@@ -6,6 +6,11 @@ import numpy as np
 
 def range01(x):
     x_out = (x-np.min(x)) / (np.max(x) - np.min(x))
+
+    ## replace any nans with 0
+    ## this can occur when an array is small (len<=2) or when the minimum equals the maximum
+    x_out[np.isnan(x_out)] = 0
+
     return(x_out)
 
     
