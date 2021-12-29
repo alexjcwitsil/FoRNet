b88e6e5ce36ca850e3b46378c69df487be73252d
alexjcwitsil <alexjcwitsil@gmail.com>
2021-12-28 23:59:51 -0500

    try using lab colorspace instead of hsv

--- functions/gen_blob_features.py
+++ functions/gen_blob_features.py
@@ -20,8 +20,14 @@ def gen_blob_features(img, gaus_sig, chan, img_meta):
     ##gray_img = img[:,:,chan]
  
     ## transfrom the image to HSV space
-    hsv_img = rgb2hsv(img)
+    # hsv_img = rgb2hsv(img)
+    # gray_img = hsv_img[:,:,chan]
+
+    ## transfer the image to lab space
+    from skimage.color import rgb2lab ## good
+    hsv_img = rgb2lab(img)
     gray_img = hsv_img[:,:,chan]
+
 
     ## scale the image between 0 and 1
     ##img = fn.img_range01(img.copy(), max=255)
